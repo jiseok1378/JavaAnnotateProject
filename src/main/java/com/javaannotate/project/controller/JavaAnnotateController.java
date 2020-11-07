5e1655f4840eca69029ce9f8b6c37a09c4fd372a
jiseok <jiseok1378@gmail.com>
2020-11-08 01:43:09 +0900

    add pdf Maven dependencies

--- src/main/java/com/javaannotate/project/controller/JavaAnnotateController.java
+++ src/main/java/com/javaannotate/project/controller/JavaAnnotateController.java
@@ -1,6 +1,11 @@
 package com.javaannotate.project.controller;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPHeaderCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.javaannotate.project.engine.InfoCollector;
 import com.javaannotate.project.engine.Print;
 import com.javaannotate.project.engine.SeparatorCalc;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -43,5 +50,27 @@ public class JavaAnnotateController {
         JSONObject result = new JSONObject();
         result.put("result", collectingFileList);
         return result;
+    }
+    @RequestMapping(value = "/pdf")
+    public String pdfCreate(HttpServletRequest req) throws Exception {
+        String fileName="";
+        String dir=System.getProperty("user.home");
+        fileName = "simple_table.pdf";
+
+        File directory = new File(dir);
+        if(!directory.exists()) directory.mkdirs(); //파일경로 없으면 생성
+
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(dir+"/"+fileName));
+
+        document.open();
+        PdfPTable table = new PdfPTable(4);
+        document.add(new Paragraph("FileName"));
+
+
+        document.newPage();
+        document.add(new Paragraph("Test2"));
+        document.close();
+        return "/techmng/tech02";
     }
 }
